4a35710404e2d1b16561b3508b9a147df941a21c
Luiz Gustavo Gavinho <lgavinho@gmail.com>
2010-08-03 13:19:20 -0400

    Conflict resolved
    Signed-off-by: Luiz Gustavo Gavinho <lgavinho@gmail.com>

--- expense/forms.py
+++ expense/forms.py
@@ -1,12 +1,31 @@
-'''
-Created on Dec 8, 2009
+#!/usr/bin/env python
+# encoding: utf-8
 
-@author: daniel
-'''
+"""
+forms.py
+Copyright (c) 2010 MoneyMe Team. All rights reserved.
+
+@author lgavinho
+@author danielmartins
+"""
 
 from django import forms
+from models import Transaction
 
 class LoginForm(forms.Form):
     login = forms.CharField(max_length=50, label=u'Login')
     passwd = forms.CharField(widget=forms.PasswordInput(),
                                 label=u'Password')
+
+
+"""
+NewExpenseForm
+- The Supplier use this form to input expense for client users.
+"""
+class NewExpenseForm(forms.Form):
+	#TODO Colocar mascara nos campos
+	moneyid = forms.CharField(label=u'MoneyID',max_length=10, required=True, widget=forms.TextInput(attrs={'size':'10','maxlength':'10'}), help_text='User client MoneyMe ID.')
+	amount = forms.DecimalField(label=u'Amount', decimal_places=2, required=True, help_text='Total expense value.')
+
+
+
